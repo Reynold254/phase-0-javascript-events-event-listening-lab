6872750138f479e498d827012856e7ed580bfbc0
Reynold254 <reynold.burudi@student.moringaschool.com>
2025-03-13 02:18:56 +0200

    What

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  // Select the button element
+  const input = document.getElementById("button");
+
+  // Define the event handler function
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  // Attach the event listener
+  input.addEventListener("click", clickAlert);
 }
